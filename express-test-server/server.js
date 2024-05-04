1320b39bd7089e7801daf311f81f023b0bc9a8af
dil4321 <dlshnchethiya@gmail.com>
2024-05-04 16:31:21 +0530

    Add encryption to GET API and done few other changes

--- express-test-server/server.js
+++ express-test-server/server.js
@@ -1,37 +1,88 @@
 //To run the server use "node server.js" command in terminal.
 
-const express = require('express');
-const bodyParser = require('body-parser');
-const CryptoJS = require('crypto-js');
-const cors = require('cors');
+const express = require("express");
+const bodyParser = require("body-parser");
+const CryptoJS = require("crypto-js");
+const cors = require("cors");
 
 const app = express();
 const port = 3001;
+const STATIC_KEY = "LECsMhEy";
+let messageData = [];
 
-// Parse JSON bodies
-app.use(bodyParser.json());
-// Enable CORS
-app.use(cors());
+app.use(bodyParser.json()); // Parse JSON bodies
+app.use(cors()); //enable CORS
 
-app.post('/create', (req, res) => {
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send("Something went wrong!");
+});
 
-    const { message } = req.body;
-    const encryptedKey = req.headers['x-timestamp'];
+app.post("/create", (req, res) => {
+  const { message } = req.body;
+  const encryptedKey = req.headers["x-timestamp"];
 
-    // Decrypt the encrypted key using the static key
-    const STATIC_KEY = "LECsMhEy";
-    const decryptedEncryptiondKey = CryptoJS.AES.decrypt(encryptedKey, STATIC_KEY).toString(CryptoJS.enc.Utf8);
-    const decodedKey = CryptoJS.enc.Base64.parse(decryptedEncryptiondKey).toString(CryptoJS.enc.Utf8);
+  // Decrypt the encrypted key using the static key
+  const decryptedEncryptiondKey = CryptoJS.AES.decrypt(
+    encryptedKey,
+    STATIC_KEY
+  ).toString(CryptoJS.enc.Utf8);
+  const decodedKey = CryptoJS.enc.Base64.parse(
+    decryptedEncryptiondKey
+  ).toString(CryptoJS.enc.Utf8);
 
 
-    decryptedMessage = CryptoJS.AES.decrypt(atob(message), decodedKey).toString(CryptoJS.enc.Utf8);//decode the base64
-    console.log('Message:', decryptedMessage);
-    console.log('Decrypted Key:', decryptedEncryptiondKey);
-    // console.log('Decrypted Timestamp:', decodedEncryptedTimestamp);
+  decryptedMessage = CryptoJS.AES.decrypt(atob(message), decodedKey).toString(
+    CryptoJS.enc.Utf8
+  );
+  
+  messageData.push(decryptedMessage);
+  console.log("Message:", messageData.at(-1));
+  
+  console.log("DATA ",messageData);
 
-    res.send('echo: ',decryptedMessage);
+  res.send(decryptedMessage);
 });
 
 app.listen(port, () => {
-    console.log(`Server running at http://localhost:${port}`);
+  console.log(`Server running at http://localhost:${port}`);
 });
+
+app.get("/get/:id", (req, res, next) => {
+  const id = req.params.id;
+  const encryptedKey = req.headers["x-timestamp"];
+  console.log("get API called ID:", id);
+
+  const message = messageData.at(id);
+  console.log("DATA ",messageData);
+  console.log(message);
+  const key = decryptKey(encryptedKey, STATIC_KEY);
+  const encryptedMessage = encryptData(message,key);
+  const msg = decryptData(encryptedMessage,key);
+
+//   res.header('X-Timestamp', encryptionKey);
+  res.json({ encryptedMessage });
+});
+
+const decryptKey = (encryptedKey, staticKey) => {
+  const decryptedEncryptiondKey = CryptoJS.AES.decrypt(
+    encryptedKey,
+    staticKey
+  ).toString(CryptoJS.enc.Utf8);
+  const decodedKey = CryptoJS.enc.Base64.parse(
+    decryptedEncryptiondKey
+  ).toString(CryptoJS.enc.Utf8);
+  return decodedKey;
+};
+
+const encryptData = (data, key) => {
+    const encryptedData = CryptoJS.AES.encrypt(data, key).toString();
+    return encryptedData;
+  };
+
+const decryptData = (encryptedData, key) => {
+  const decryptedData = CryptoJS.AES.decrypt(encryptedData, key).toString(
+    CryptoJS.enc.Utf8
+  );
+  return decryptedData;
+};
--- security-integrated-react-application/src/components/FormComponent.js
+++ security-integrated-react-application/src/components/FormComponent.js
@@ -1,65 +1,112 @@
+//uncomment console logs if you want to verify the encoding/decoding process.
 import React, { useState } from "react";
 import axios from "axios";
 import CryptoJS from "crypto-js";
 
+
 const FormComponent = () => {
-  const [inputValue, setInputValue] = useState("");
+  const [inputNote, setInputNote] = useState("");
+  const [inputId, setInputId] = useState("");
   const STATIC_KEY = "LECsMhEy";
 
-  const handleChange = (e) => {
-    setInputValue(e.target.value);
+  const handleNoteChange = (e) => {
+    setInputNote(e.target.value);
+  };
+  const handleIdChange = (e) => {
+    setInputId(e.target.value);
   };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    console.log(inputValue);
+    console.log(inputNote);
     const encryptionKey = generateEncryptionKey();
-    console.log("Generated Encryption Key=> ",encryptionKey);
+    console.log("Generated Encryption Key=> ", encryptionKey);
 
     // Example usage encrypt
-    const encryptedData = encryptData(inputValue, encryptionKey);
-    console.log("Encrypted Data=> ",encryptedData);
+    const encryptedData = encryptData(inputNote, encryptionKey);
+    // console.log("Encrypted Data=> ", encryptedData);
 
     // Example usage encrypted data encoding with Base64
     const encodedData = encode(encryptedData);
-    console.log("Encoded Data=> ",encodedData);
+    // console.log("Encoded Data=> ", encodedData);
 
     // Example usage decrypt
     const decryptedData = decryptData(encodedData, encryptionKey);
-    console.log("Decrypted Data on client side=> ",decryptedData);
+    // console.log("Decrypted Data on client side=> ", decryptedData);
 
-    const encodedKey = encode(encryptionKey);
-    console.log("encKey",encodedKey);
-    const encryptedEncryptionKey = encryptData(encodedKey,STATIC_KEY);
-    console.log("Encrypted encryption key on client side=> ",encryptedEncryptionKey);
-
+    const encryptedKey = encryptKey(encryptionKey, STATIC_KEY);
+    // console.log(
+    //   "Encrypted encryption key on client side=> ",
+    //   encryptedKey
+    // );
 
     try {
-        const response = await axios.post('http://localhost:3001/create', {
-            message: encodedData,
-        }, {
-            headers: {
-                'X-Timestamp': encryptedEncryptionKey,
-            },
-        });
-        console.log(response.data);
-        // Handle success, show message, etc.
+      const response = await axios.post(
+        "http://localhost:3001/create",
+        {
+          message: encodedData,
+        },
+        {
+          headers: {
+            "X-Timestamp": encryptedKey,
+          },
+        }
+      );
+      console.log(response.data);
+      // Handle success, show message, etc.
     } catch (error) {
-        console.error(error);
-        // Handle error, show error message, etc.
+      console.error(error);
+      // Handle error, show error message, etc.
     }
   };
 
+  const handleGet = async (e) => {
+    e.preventDefault();
+    const encryptionKey = generateEncryptionKey();//generate new key to send. This key is use by the server to encrypt the data that send to the client side.
+    const encryptedKey = encryptKey(encryptionKey,STATIC_KEY);
+    //formating the url.
+    const url = `http://localhost:3001/get/${inputId}#${inputId}`;
+
+    //get the note from the server using note id, decrypt it and show on alert box.
+    axios
+      .get(url,{
+        headers: {
+          'X-Timestamp': encryptedKey
+      }
+      })
+      .then((response) => {
+        const encryptedMessage = response.data.encryptedMessage;
+
+        const decryptedNote = decryptGetAPIData(encryptedMessage,encryptionKey);
+        console.log("Note:", decryptedNote);
+        window.alert(`Note received: ${decryptedNote}`);
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  };
+
   return (
-    <form onSubmit={handleSubmit}>
-      <input
-        type="text"
-        value={inputValue}
-        onChange={handleChange}
-        placeholder="Enter data"
-      />
-      <button type="submit">Submit</button>
-    </form>
+    <>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={inputNote}
+          onChange={handleNoteChange}
+          placeholder="Enter data"
+        />
+        <button type="submit">Submit Note</button>
+      </form>
+      <form onSubmit={handleGet}>
+        <input
+          type="number"
+          value={inputId}
+          onChange={handleIdChange}
+          placeholder="Enter id"
+        />
+        <button type="submit">Get Note</button>
+      </form>
+    </>
   );
 };
 
@@ -76,16 +123,37 @@ const generateEncryptionKey = () => {
 };
 
 const encryptData = (data, key) => {
-  const encrypted = CryptoJS.AES.encrypt(data, key).toString();
-  return encrypted;
+  const encryptedData = CryptoJS.AES.encrypt(data, key).toString();
+  return encryptedData;
 };
 
 const decryptData = (encryptedData, key) => {
-    const decrypted = CryptoJS.AES.decrypt(atob(encryptedData), key).toString(CryptoJS.enc.Utf8);
-    return decrypted;
+  const decryptedData = CryptoJS.AES.decrypt(atob(encryptedData), key).toString(
+    CryptoJS.enc.Utf8
+  );
+  return decryptedData;
 };
 
 const encode = (encrypted) => {
-    const encoded = btoa(encrypted); // Convert data to Base64
-    return encoded;
+  const encoded = btoa(encrypted); // Convert data to Base64
+  return encoded;
+};
+
+const encryptKey = (encriptionKey,staticKey) => {
+  const encoded = btoa(encriptionKey); // Convert data to Base64
+  const encryptedKey = CryptoJS.AES.encrypt(encoded, staticKey).toString();
+  return encryptedKey;
 }
+
+const decryptKey = (encryptedKey,staticKey) => {
+  const decryptedEncryptiondKey = CryptoJS.AES.decrypt(encryptedKey, staticKey).toString(CryptoJS.enc.Utf8);
+  const decodedKey = CryptoJS.enc.Base64.parse(decryptedEncryptiondKey).toString(CryptoJS.enc.Utf8);
+  return decodedKey;
+}
+
+const decryptGetAPIData = (encryptedData, key) => {
+  const decryptedData = CryptoJS.AES.decrypt(encryptedData, key).toString(
+    CryptoJS.enc.Utf8
+  );
+  return decryptedData;
+};
